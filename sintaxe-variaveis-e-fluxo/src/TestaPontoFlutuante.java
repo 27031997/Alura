3137d40c538cf55e7cbeec3f084ede08c90758cb
Anna Inácio <55771360+27031997@users.noreply.github.com>
2020-08-26 20:29:07 -0300

    Java 1.1

---
+++ sintaxe-variaveis-e-fluxo/src/TestaPontoFlutuante.java
@@ -0,0 +1,15 @@
+
+public class TestaPontoFlutuante {
+	
+	public static void main(String[] args) {
+		
+		double salario = 1250.70;
+		System.out.println("Salário é: " + salario);
+
+        int divisao = 5 / 2;
+        System.out.println(divisao);
+        
+        double divisaoDouble = 5.0 / 2;
+        System.out.println(divisaoDouble);
+	}
+}
